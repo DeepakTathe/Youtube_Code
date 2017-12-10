c85cb3232327c6f820721d84d876603dc6f2dee7
DeepakTathe <34350111+DeepakTathe@users.noreply.github.com>
2017-12-10 13:15:26 +0530

    Add files via upload

---
+++ Linked list.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+struct node{
+int data;
+struct node *next;
+};
+
+node *start=NULL;
+
+node *createNode(){
+node *temp;
+temp = new node;
+return temp;
+}
+
+void insertNode(){
+node *temp,*traverse;
+int n;
+cout<<"Enter -1 to End"<<endl;
+cout<<"Enter the values to be added in List"<<endl;
+cin>>n;
+while( n!=-1){
+    temp = createNode();
+    temp->data = n;
+    temp->next = NULL;
+
+    if(start == NULL){
+        start = temp;
+    }
+    else{
+        traverse = start;
+        while(traverse->next != NULL){
+            traverse= traverse->next;
+        }
+        traverse->next= temp;
+        }
+   cout<<"Enter the value to be added in the list"<<endl;
+   cin>>n;
+
+}
+}
+void printlist(){
+node *traverse = start;
+while(traverse != NULL){
+
+    cout<<traverse->data<<" ";
+    traverse = traverse->next;
+}
+}
+int main(){
+
+  int option;
+  do{
+    cout<<"---------------Main Menu-------------------"<<endl;
+    cout<<"1 :Create List"<<endl;
+    cout<<"2. Print List"<<endl;
+    cout<<"3. Exit"<<endl;
+    cin>>option;
+
+    switch(option){
+    case 1:insertNode();
+           break;
+    case 2:printlist();
+           break;
+    }
+  }while(option != 3);
+
+}
